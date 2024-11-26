5cbb44d2a1187d70a45a6c08f284abad7b80ef50
SHUBHAM KUMAR <shubhamkumar943150@gmail.com>
2024-11-26 20:32:41 +0530

    Add files via upload

---
+++ index.js
@@ -0,0 +1,11 @@
+var typed= new Typed(".many-text", {
+    strings: ["Grow your Skill","Learn", "Collaborate",
+     "Fostering Innovation"],
+    typeSpeed: 200,
+    backSpeed: 200,
+    backDelay: 1000,
+    loop: true
+    
+})
+
+
